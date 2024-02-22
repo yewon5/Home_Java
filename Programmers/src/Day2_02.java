d9cb9268861a528b481c404896207304d7063201
PC <PC@222.98.60.222>
2024-02-22 23:50:07 +0900

    Programmers

---
+++ Programmers/src/Day2_02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Day2_02 {
+	public static void main(String[] args) {
+		// TODO 문자열 붙여서 출력하기
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.print(a+b);
+    }
+}
